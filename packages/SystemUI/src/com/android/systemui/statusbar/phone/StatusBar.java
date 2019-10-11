b90c18c149b3cb19e441d281f9cb75339d3c5e33
Ali B <abittin@gmail.com>
2019-10-11 20:04:08 +0300

    SystemUI: QS and QQS layout fixes
    Change-Id: Ifc94a997ccebda435b06175a6a35c8354196b451

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3899,6 +3899,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mQSPanel != null) {
             mQSPanel.updateResources();
             mQSPanel.updateSettings();
+        }
+        if (mQuickQSPanel != null) {
             mQuickQSPanel.updateSettings();
         }
     }
